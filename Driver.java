649a1d73f2b8899fd6112fe127d9dfc609d7e332
Josh <eeei3.joshua0307lol@gmail.com>
2024-09-29 12:41:57 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,55 @@
+import java.io.File;
+import java.util.Arrays;
+
 public class Driver {
     public static void main(String [] args) {
-	Polynomial p = new Polynomial();
-	System.out.println(p.evaluate(3));
-	double [] c1 = {6,0,0,5};
-	Polynomial p1 = new Polynomial(c1);
-	double [] c2 = {0,-2,0,0,-9};
-	Polynomial p2 = new Polynomial(c2);
-	Polynomial s = p1.add(p2);
-	System.out.println("s(0.1) = " + s.evaluate(0.1));
-	if(s.hasRoot(1))
-	    System.out.println("1 is a root of s");
-	else
-	    System.out.println("1 is not a root of s");
-    }
+//		File f = new File("bruh8");
+//		Polynomial p1 = new Polynomial(f);
+//		System.out.println(p1.evaluate(4));
+//		double [] c1 = {-4, 0, 9};
+//		Polynomial p2 = new Polynomial();
+//		Polynomial p3 = p1.multiply(p2);
+//		p3.saveToFile("bruh9");
+//		File f = new File("bruh6");
+//		Polynomial p1 = new Polynomial(f);
+//		Polynomial p2 = new Polynomial(c2);
+//		Polynomial p3 = p1.add(p2);
+//		p3.saveToFile("bruh3");
+//		System.out.println(Arrays.toString("x2".split("[x]")));
+//		System.out.println(p1.hasRoot(2.0));
+//		p1.saveToFile("bruh7");
+
+
+
+
+		Polynomial p = new Polynomial();
+		System.out.println(p.evaluate(3));
+		double [] c1 = {6,0,0,5};
+		Polynomial p1 = new Polynomial(c1);
+		double [] c2 = {0,-2,0,0,-9};
+		Polynomial p2 = new Polynomial(c2);
+		Polynomial s = p1.add(p2);
+		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		s.saveToFile("bruh1");
+		if(s.hasRoot(1)) {
+			System.out.println("1 is a root of s");
+		}
+		else {
+			System.out.println("1 is not a root of s");
+		}
+		File funfile = new File("bruh5");
+		Polynomial p3 = new Polynomial(funfile);
+		System.out.println("p3(2) = " + p3.evaluate(2.0));
+		Polynomial p4 = p1.multiply(p2);
+		p4.saveToFile("bruh1");
+		double [] c3 = {2, 1};
+		double [] c4 = {-2, 1};
+		Polynomial p5 = new Polynomial(c3);
+		Polynomial p6 = new Polynomial(c4);
+		Polynomial p7 = p5.multiply(p6);
+		System.out.println(p7.evaluate(2.0));
+		p7.saveToFile("bruh2");
+
+	}
+
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,59 +1,258 @@
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Polynomial {
-    double [] coef;
+    double [] nonzcoef;
+    int [] expo;
+
 
     public Polynomial() {
-	coef = new double[0];
+	nonzcoef = new double[0];
+	expo = new int[0];
+    }
+
+    public Polynomial(File fp) {
+	try {
+		BufferedReader input = new BufferedReader(new FileReader(fp));
+		String tmp = input.readLine();
+		tmp = tmp.replaceAll("-", "+-");
+		String p = "[+]";
+		String var = "[x]";
+		String[] terms = tmp.split(p);
+		int j = terms.length;
+		for (String i: terms) {
+			if (i.isEmpty())
+				j--;
+
+		}
+		nonzcoef = new double[j];
+		expo = new int[j];
+
+		j = 0;
+
+		for (String i : terms) {
+			if (i.contains("x")) {
+				if (i.split(var).length == 2) {
+					if (i.split(var)[0].equals("-"))
+						nonzcoef[j] = -1;
+					else if (!i.split(var)[0].isEmpty())
+						nonzcoef[j] = Double.parseDouble(i.split(var)[0]);
+					else
+						nonzcoef[j] = 1;
+					expo[j] = Integer.parseInt(i.split(var)[1]);
+				}
+				else {
+					if (i.startsWith(i.split(var)[0])) {
+						if (Double.parseDouble(i.split(var)[0]) == 0)
+							j--;
+						else {
+							if (i.split(var)[0].equals("-"))
+								nonzcoef[j] = -1;
+							else
+								nonzcoef[j] = Double.parseDouble(i.split(var)[0]);
+							expo[j] = 1;
+							}
+						}
+					else {
+						nonzcoef[j] = 1;
+						expo[j] = Integer.parseInt(i.split(var)[0]);
+					}
+				}
+			}
+			else if (!(i.isEmpty())) {
+				if (Double.parseDouble(i) == 0)
+					j--;
+				else {
+					nonzcoef[j] = Double.parseDouble(i);
+					expo[j] = 0;
+				}
+			}
+			else {
+				j--;
+			}
+			j++;
+		}
+	}
+	catch (IOException e) {
+		nonzcoef = new double[0];
+		expo = new int[0];
+		}
+    }
+
+    public Polynomial(HashMap<Integer, Double> input) {
+		if (input != null) {
+			nonzcoef = new double[input.size()];
+			expo = new int[input.size()];
+			int j = 0;
+			for (int i : input.keySet()) {
+				nonzcoef[j] = input.get(i);
+				expo[j] = i;
+				j++;
+			}
+		}
+		else {
+			nonzcoef = new double[0];
+			expo = new int[0];
+		}
     }
 
     public Polynomial(double [] input) {
-	int len = input.length;
-	coef = new double[len];
-	int i = 0;
-	while (i < len) {
-	    coef[i] += input[i];
-	    i++;
-	}
+		if (input != null) {
+			int i = 0;
+			int newlen = 0;
+			while (i < input.length) {
+				if (input[i] != 0) {
+					newlen++;
+				}
+				i++;
+			}
+			nonzcoef = new double[newlen];
+			expo = new int[newlen];
+			int j = 0;
+			i = 0;
+			while (i < input.length) {
+				if (input[i] != 0) {
+					nonzcoef[j] += input[i];
+					expo[j] += i;
+					j++;
+				}
+				i++;
+			}
+		}
+		else {
+			nonzcoef = new double[0];
+			expo = new int[0];
+		}
+    }
+
+    private HashMap<Integer, Double> polytohash(Polynomial input) {
+		HashMap<Integer, Double> res = new HashMap<Integer, Double>();
+		int len = input.expo.length;
+		int i = 0;
+		while (i < len) {
+	    	res.put(input.expo[i], input.nonzcoef[i]);
+		}
+		return res;
     }
 
     public Polynomial add(Polynomial input) {
-	int len1 = input.coef.length;
-	int len2 = coef.length;
-	double [] sol = new double[Math.max(len1, len2)];
-	int i = 0;
-	while (i < len2) {
-	    sol[i] += coef[i];
-	    i++;
-	}
-	i = 0;
-	while (i < len1) {
-	    sol[i] += input.coef[i];
-	    i++;
-	}
-	i = 0;
-	return new Polynomial(sol);
+		if (input == null)
+			return null;
+		HashMap<Integer, Double> res = new HashMap<Integer, Double>();
+		int len1 = input.expo.length;
+		int len2 = expo.length;
+		int i = 0;
+		while (i < len1) {
+	    	if (res.get(input.expo[i]) == null)
+				res.put(input.expo[i], input.nonzcoef[i]);
+	    	else {
+				res.replace(input.expo[i], res.get(i) + input.nonzcoef[i]);
+	    	}
+	    	i++;
+		}
+		i = 0;
+		while (i < len2) {
+	    	if (res.get(this.expo[i]) == null)
+				res.put(this.expo[i], this.nonzcoef[i]);
+	    	else {
+				double a = res.get(i) + this.nonzcoef[i];
+				if (a != 0.0)
+					res.replace(this.expo[i], a);
+				else
+					res.remove(this.expo[i]);
+			}
+	    	i++;
+		}
+		if (res.size() == 0)
+			res.put(0, 0.0);
+		return new Polynomial(res);
     }
 
     public double evaluate(double x) {
-	double ans = 0;
-	int i = 0;
-	int len = coef.length;
-	while (i < len) {
-	    ans = ans + Math.pow(x, i) * coef[i];
-	    i++;
-	}
-	return ans;
+		double ans = 0;
+		int i = 0;
+		int len = expo.length;
+		while (i < len) {
+			if (expo[i] == 1) {
+				ans = ans + x * nonzcoef[i];
+			}
+			else {
+				ans = ans + Math.pow(x, expo[i]) * nonzcoef[i];
+			}
+	    	i++;
+		}
+		return ans;
+    }
+
+    public Polynomial multiply(Polynomial input) {
+		if (input == null)
+			return null;
+		HashMap<Integer, Double> res = new HashMap<Integer, Double>();
+		int i = 0;
+		while (i < expo.length) {
+			int j = 0;
+			while (j < input.expo.length){
+				if (res.containsKey(input.expo[j] + expo[i])) {
+					double a = res.get(input.expo[j] + expo[i]) + nonzcoef[i] * input.nonzcoef[j];
+					if (a != 0.0)
+						res.put(input.expo[j] + expo[i], a);
+					else
+						res.remove(input.expo[j] + expo[i]);
+				}
+				else
+					res.put(input.expo[j] + expo[i], nonzcoef[i] * input.nonzcoef[j]);
+				j++;
+			}
+			i++;
+		}
+
+		return new Polynomial(res);
+    }
+
+    public void saveToFile(String file) {
+		try {
+			File nfile = new File(file);
+			if (!nfile.createNewFile())
+				;
+			FileWriter nwriter = new FileWriter(file, false);
+			int i = 0;
+			while (i < expo.length) {
+				if (i != 0) {
+					if (nonzcoef[i] > 0) {
+						nwriter.write("+");
+					}
+				}
+				nwriter.write(String.valueOf(nonzcoef[i]));
+				if (expo[i] != 0) {
+					nwriter.write("x");
+					nwriter.write(String.valueOf(expo[i]));
+				}
+				i++;
+			}
+			if (i == 0) {
+				nwriter.write("0");
+			}
+			nwriter.close();
+		}
+		catch (IOException e) {
+			return;
+		}
     }
 
     public boolean hasRoot(double x) {
-	double ans = 0;
-	int i = 0;
-	int len = coef.length;
-	while (i < len) {
-	    ans = ans + Math.pow(x, i) * coef[i];
-	    i++;
-	}
-	if (ans == 0.0)
-	    return true;
-	return false;
+		double ans = 0;
+		int i = 0;
+		int len = nonzcoef.length;
+		while (i < len) {
+	    	ans = ans + Math.pow(x, expo[i]) * nonzcoef[i];
+	    	i++;
+		}
+		if (ans == 0.0)
+	    	return true;
+		return false;
     }
 }
